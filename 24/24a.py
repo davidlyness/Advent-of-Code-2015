a9224b993b50d0af337af6bc652418ddbdcad590
David Lyness <david@davidlyness.com>
2015-12-24 09:49:51 +0000

    Add Day 24 solution

---
+++ 24/24a.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""Advent of Code 2015, Day 24, Part 1"""
+
+import itertools
+
+
+def qe(group):
+    """
+    Calculates the QE (product) of a group. Because product(n) doesn't exist... :(
+    :param group: the reference group
+    :return: the QE of a group
+    """
+    p = 1
+    for n in group:
+        p *= n
+    return p
+
+
+with open("input.txt") as f:
+    packages = set([int(x) for x in f.read().splitlines()])
+
+required_group_weight = sum(packages) / 3
+best_qe = None
+length_group_1 = 1
+
+while best_qe is None and length_group_1 < len(packages):
+    for combo_group_1 in itertools.combinations(packages, length_group_1):
+        if sum(combo_group_1) == required_group_weight:
+            remaining_packages_two = packages - set(combo_group_1)
+            for length_group_2 in range(len(remaining_packages_two)):
+                for combo_group_2 in itertools.combinations(remaining_packages_two, length_group_2):
+                    if sum(combo_group_2) == required_group_weight:
+                        combo_group_3 = remaining_packages_two - set(combo_group_2)
+                        if sum(combo_group_3) == required_group_weight:
+                            current_qe = qe(combo_group_1)
+                            if best_qe is None or current_qe < best_qe:
+                                best_qe = current_qe
+    length_group_1 += 1
+
+print(best_qe)
