644986a18f9133fe3f2ee61c2835c3c896a2a0f5
David Lyness <david@davidlyness.com>
2015-12-24 17:38:43 +0000

    Refactor grouping calculation to explicitly return optimal answer

--- 24/24b.py
+++ 24/24b.py
@@ -16,29 +16,32 @@ def qe(group):
     return p
 
 
+def calculate_optimal_groupings(packages):
+    """
+    Calculates the optimal groupings for a given set of packages.
+    :param packages: reference set of packages to group
+    :return: QE value of optimal arrangement
+    """
+    required_group_weight = sum(packages) / 4
+    length_group_1 = 1
+    while length_group_1 < len(packages):
+        for combo_group_1 in itertools.combinations(packages, length_group_1):
+            if sum(combo_group_1) == required_group_weight:
+                remaining_packages_two = packages - set(combo_group_1)
+                for length_group_2 in range(len(remaining_packages_two)):
+                    for combo_group_2 in itertools.combinations(remaining_packages_two, length_group_2):
+                        if sum(combo_group_2) == required_group_weight:
+                            remaining_packages_three = remaining_packages_two - set(combo_group_2)
+                            for length_group_3 in range(len(remaining_packages_three)):
+                                for group_combo_3 in itertools.combinations(remaining_packages_three, length_group_3):
+                                    if sum(group_combo_3) == required_group_weight:
+                                        group_combo_4 = remaining_packages_three - set(group_combo_3)
+                                        if sum(group_combo_4) == required_group_weight:
+                                            return qe(combo_group_1)
+        length_group_1 += 1
+    return -1
+
 with open("input.txt") as f:
-    packages = set([int(x) for x in f.read().splitlines()])
+    package_set = set([int(x) for x in f.read().splitlines()])
 
-required_group_weight = sum(packages) / 4
-best_qe = None
-length_group_1 = 1
-
-while best_qe is None and length_group_1 < len(packages):
-    for combo_group_1 in itertools.combinations(packages, length_group_1):
-        if sum(combo_group_1) == required_group_weight:
-            remaining_packages_two = packages - set(combo_group_1)
-            for length_group_2 in range(len(remaining_packages_two)):
-                for combo_group_2 in itertools.combinations(remaining_packages_two, length_group_2):
-                    if sum(combo_group_2) == required_group_weight:
-                        remaining_packages_three = remaining_packages_two - set(combo_group_2)
-                        for length_group_3 in range(len(remaining_packages_three)):
-                            for group_combo_3 in itertools.combinations(remaining_packages_three, length_group_3):
-                                if sum(group_combo_3) == required_group_weight:
-                                    group_combo_4 = remaining_packages_three - set(group_combo_3)
-                                    if sum(group_combo_4) == required_group_weight:
-                                        current_qe = qe(combo_group_1)
-                                        if best_qe is None or current_qe < best_qe:
-                                            best_qe = current_qe
-    length_group_1 += 1
-
-print(best_qe)
+print(calculate_optimal_groupings(package_set))
